d1f77f56802d26090f2896d69570428ed9cf3737
Tiago <tiagocondack@gmail.com>
2025-07-18 18:12:54 -0300

    Atualizar o urls.py

--- agendafacil/agendafacil/agendafacil/urls.py
+++ agendafacil/agendafacil/agendafacil/urls.py
@@ -5,5 +5,7 @@ from app_agenda_facil import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('', views.perfil, name='perfil'),
+
 ]
