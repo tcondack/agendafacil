38c1988016b01058cccbb555b3e5496ec0e7b564
tcondack <tiagocondack@gmail.com>
2025-07-20 13:48:27 -0300

    updates gerais.

--- agendafacil/agendafacil/agendafacil/settings.py
+++ agendafacil/agendafacil/agendafacil/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -104,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
@@ -117,6 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static")
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- agendafacil/agendafacil/agendafacil/urls.py
+++ agendafacil/agendafacil/agendafacil/urls.py
@@ -2,11 +2,13 @@
 from django.contrib import admin
 from django.urls import path
 from app_agenda_facil import views
+from django.urls import include, url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('', views.perfil, name='perfil'),
+    path('', include(admin.site.urls)),
     path('', views.adm_perfil, name='adm_perfil')
 
 ]
